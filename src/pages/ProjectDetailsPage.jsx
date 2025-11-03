237381c85062e7fc42d10fda9a133ad5965ea8d6
Manayer <manayerbusiness@gmail.com>
2025-11-03 11:27:49 +0300

    fetch, display & delete project tasks in ProjectDetailsPage.jsx

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getProjectDetails } from '../api'; 
+import { getProjectDetails, getProjectsTasks,deleteTask } from '../api'; 
 
 const ProjectDetailsPage = () => {
     const { id } = useParams(); 
@@ -8,14 +8,50 @@ const ProjectDetailsPage = () => {
     const [project, setProject] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [tasks, setTasks] = useState([])
+    const fetchProjectAndTasks = async () => {
+        try{
+            const [projectData, taskData] = await Promise.all([
+                getProjectDetails(id),
+                getProjectsTasks(id)
+            ])
+            setProject(projectData)
+            setTasks(taskData)
+        }catch (err){
+            if(err.response && err.response.status === 401){
+                localStorage.removeItem('access_token')
+                localStorage.removeItem('refresh_token')
+                navigate('/login')
+            }else if (err.response && err.response.status === 404){
+                setError('Project Not Found')
+            }else{
+                setError('Failed to load project details or tasks')
+            }
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const handleTaskDelete = async (taskId) =>{
+        const confirmed = window.confirm('Are you sure you want to delete this task?')
+        if(confirmed){
+            try{
+                await deleteTask(id,taskId)
+                setTasks(tasks.filter(task => task.id !== taskId))
+            }catch (error){
+                alert('Failed to delete task. check console for details')
+            }
+        }
+    }
 
     useEffect(() => {
+        fetchProjectAndTasks()
         setIsLoading(true);
         getProjectDetails(id)
             .then(data => {
                 setProject(data);
                 setIsLoading(false);
-            })
+            },[id])
             .catch(err => {
                 setIsLoading(false);
                 if (err.response && err.response.status === 401) {
@@ -47,7 +83,29 @@ const ProjectDetailsPage = () => {
             <h1>Project: {project.title}</h1>
             <p><strong>ID:</strong> {project.id}</p>
             <p><strong>Description:</strong> {project.description || 'No description provided.'}</p>
-            
+            <hr />
+            <h2>Poject Tasks ({tasks.length})</h2>
+
+            {/* Task form here later */}
+            <div className='task-list'>
+                {tasks.length>0 ?(
+                    <ul>
+                        {tasks.map(task =>(
+                            <li key={task.id} className={`task-item task-${task.status}`}>
+                                <strong>{task.title}</strong>
+                                <span>Status: {task.status}</span>
+                                <span>Due: {task.due_date}</span>
+                                <span>Assigned to: **{task.assigned_to ? task.assigned_to.username : 'Unassigned'}**</span>
+                                <button onClick={() => handleTaskDelete(task.id)} className="delete-btn">
+                                    
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                ): (
+                    <p>No tasks found for this project. Start by adding one!</p>
+                )}
+            </div>
         </div>
     );
 };
