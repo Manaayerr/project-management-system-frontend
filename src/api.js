19605b8490b950fef0d972d13b79ddf1c0757f37
Manayer <manayerbusiness@gmail.com>
2025-11-04 09:59:12 +0300

    createProject

--- src/api.js
+++ src/api.js
@@ -1,6 +1,16 @@
 import axiosInstance from "./api/axiosInstance";
 // const API_BASE = "http://127.0.0.1:8000/api";
 
+export const createProject = async (projectData =>{
+  try{
+    const response = await axiosInstance.post(`/projects`,projectData)
+    return response.data
+  }catch(error){
+    console.error('Error creating project:' error.response?.data || error.message)
+    throw error
+  }
+})
+
 export const getProjects = async () => {
   try {
     const response = await axiosInstance.get(`projects/`);
@@ -108,3 +118,4 @@ export const deleteTask = async (projectId,taskId) => {
     throw error
   }
 }
+
